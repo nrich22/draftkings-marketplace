77e037fb72ec832b44f2e72a7444b24f9b9d4976
Nicholas Ricciarelli <nar5501@rit.edu>
2022-04-12 19:07:36 -0400

    routes

--- backend/DraftKingsMarketplace.Api/DraftKingsMarketplace.Api/Controllers/DraftKingsController.cs
+++ backend/DraftKingsMarketplace.Api/DraftKingsMarketplace.Api/Controllers/DraftKingsController.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using DraftKingsMarketplace.Api.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace DraftKingsMarketplace.Api.Controllers
 {
     [ApiController]
-    [Route("v1")]
+    [Route("marketplace")]
+    [Authorize]
     public class DraftKingsController : ControllerBase
     {
         private readonly ILogger<DraftKingsController> _logger;
@@ -19,6 +21,7 @@ namespace DraftKingsMarketplace.Api.Controllers
         }
 
         [HttpGet]
+        [Route("drops")]
         public IEnumerable<CollectibleDrop> Get()
         {
             return null;
